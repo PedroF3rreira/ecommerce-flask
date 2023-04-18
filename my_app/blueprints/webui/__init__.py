0248ae2ef13a3395872d80de8953da70466f6267
Pedro Daniel <pedro-edu19@outlook.com>
2023-04-18 11:39:53 -0300

    adicionando listagem de modelos

--- my_app/blueprints/webui/__init__.py
+++ my_app/blueprints/webui/__init__.py
@@ -1,12 +1,14 @@
 from flask import Blueprint
 
-from .admin import index, user
+from .admin import *
 
 bp = Blueprint("webui", __name__, template_folder="templates")
 
 # ADMIN VIEWS
 bp.add_url_rule("/admin", view_func=index, )
 bp.add_url_rule("/admin/usuario", view_func=user, )
+bp.add_url_rule("/admin/fornecedor", view_func=provider, )
+bp.add_url_rule("/admin/categoria", view_func=category, )
 
 
 
--- my_app/blueprints/webui/admin.py
+++ my_app/blueprints/webui/admin.py
@@ -1,12 +1,14 @@
 from flask import abort, render_template
 from my_app.blueprints.models.user import User
+from my_app.blueprints.models.provider import Provider
+from my_app.blueprints.models.category import Category
 from my_app.ext.database import db
 
 
 def index():
 	return render_template('admin/index.html')
 
-
+# List Users
 def user():
 	users = User.query.all()
 	columns = {
@@ -15,3 +17,27 @@ def user():
 		'email': 'Email',
 	}
 	return render_template('admin/list.html', items=users, columns=columns)
+
+# List Providers
+def provider():
+	providers = Provider.query.all()
+	columns = {
+		'id': 'Id',
+		'name': 'Nome fantasia',
+		'corporate_name': 'Razão social',
+		'email': 'Email',
+		'phone': 'Contato',
+	}
+	return render_template('admin/list.html', items=providers, columns=columns)
+
+
+# List Category
+def category():
+	categories = Category.query.all()
+	columns = {
+		'id': 'Id',
+		'description': 'Nome',
+		'long_description': 'Observações',
+		
+	}
+	return render_template('admin/list.html', items=categories, columns=columns)
